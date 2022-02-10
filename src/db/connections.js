42d86f2a5d51a1a5c4a45671237109ddb1f58d36
simonamarcu2 <supersimy16@gmail.com>
2022-02-10 10:45:04 +0000

    sequelize and authenticate connection

---
+++ src/db/connections.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+let sequelize = new Sequelize(process.env.MYSQL_URI);
+
+// sequelize.authenticate().then(() => {
+//   console.log("connected to db")
+// }).catch((err) => {
+//   console.log(err)
+// })
+
+//OR to authenticate the connection
+
+// let connection = async() => {
+//   try {
+//     await sequelize.authenticate()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// connection();
+
+
+
+
+module.exports = sequelize;
