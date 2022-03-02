73624e5ca4765ca3a009168f8d71e421f2217c04
simonamarcu2 <supersimy16@gmail.com>
2022-03-02 09:30:36 +0000

    app updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
-const { addFilm, list } = require("./Movies/movieMethod");
+const { addFilm, list, find, update, destroy } = require("./Movies/movieMethod");
 const argv = yargs(hideBin(process.argv)).argv;
 
 const app = async () => {
@@ -12,6 +12,16 @@ const app = async () => {
     await addFilm(filmObj)
   } else if (argv.list) {
     await list()
+  } else if (argv.find) {
+    const searchObj = objFromArrs(argv.keys, argv.values);
+    await find(searchObj);
+  } else if (argv.update) {
+    const searchObj = objFromArrs(argv.keys, argv.values);
+    const updateObj = objFromArrs(argv.updateKeys, argv.updateValues);
+    await update(searchObj, updateObj);
+  } else if (argv.delete) {
+    const deleteObj = objFromArrs(argv.keys, argv.values);
+    await destroy(deleteObj);
   } else {
     console.log("Incorrect command")
   }
